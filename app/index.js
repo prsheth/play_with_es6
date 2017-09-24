e63a9ab5f321f9efb00d310208cf13e5832bbcb1
pranay <pranayrsheth@gmail.com>
2017-09-24 00:21:11 -0400

    Variables, constants and template literals in ES6

--- app/index.js
+++ app/index.js
@@ -1,1 +1,16 @@
-console.log('Hello World is running using webpack and babel');
+//LET is used instaed of var
+let a ="hello";
+console.log(a);
+{
+    let a ='world';
+    console.log(a);// Both values of a will defer as this one is under a block. Block scpoing is the reason why this value will be destoryed out side the scope
+}
+
+//CONST is a constant and the value persists through the compilation , the value of b is readonly   
+const b=3;
+
+//STRING MANIPULATION
+//concatination
+let d=`hello ${a}`; //Template literals to make most common code ${} are used for variables replacing the + singn and the new charater ` is the back ticked string
+console.log(d);
+
